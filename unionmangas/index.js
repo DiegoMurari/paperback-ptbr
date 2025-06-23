4b982145e374f26b3b3b33d22e2674bf054b3000
DiegoMurari <127155939+DiegoMurari@users.noreply.github.com>
2025-06-23 11:10:03 -0300

    Criar index.js

---
+++ unionmangas/index.js
@@ -0,0 +1,5 @@
+export const id = "unionmangas";
+export const name = "Union Mangás";
+export const version = "1.0.0";
+export const website = "https://unionmangas.top";
+export const language = ["pt-BR"];
